9501f34ca0e54a2a9b3962ef86c388515724f24d
keea <heea44@naver.com>
2024-08-04 06:21:11 +0900

    ✨ feat: #3 랜드마크 데이터 Unity에 전달

--- Assets/WUMix/Scripts/TestWeb.cs
+++ Assets/WUMix/Scripts/TestWeb.cs
@@ -5,6 +5,11 @@ public class TestWeb : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        WebGLHandLandmarksPlugin.Init(960, 600);
+        WebGLHandLandmarksPlugin.Init(960, 600, OnLandmarksUpdate);
+    }
+
+    public void OnLandmarksUpdate(string multiHandLandmarks, int width, int height)
+    {
+        Debug.Log("OnLandmarksUpdate : " + multiHandLandmarks);
     }
 }
